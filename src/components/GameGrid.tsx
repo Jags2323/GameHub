5bb1118d4a037b9dbc1b2d4ca35e48fe574bcdaf
Vikram Rajeev Reddy Kadire <vikramrajeevreddykadire@public-148.fa.farmlib.org>
2023-05-06 10:43:23 -0400

    Image rendering basic

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import apiClient from '../services/api-client'
+import { Text } from '@chakra-ui/react'
 
 interface Game{
     id: number,
     name: string,
-    rating: number
+    rating: number,
+    background_image: string
 }
 
 interface FetchGameresponse{
@@ -12,22 +14,35 @@ interface FetchGameresponse{
     results: Game[]
 }
 
+interface Props{
+    game: Game
+}
+
+const GameCard = ({ game } : Props) => {
+    return (
+      <div className="game-card">
+        <img width='500px' src={game.background_image} alt={game.name} />
+        <h2>{game.name}</h2>
+        <p>{game.rating}</p>
+      </div>
+    );
+};
+  
 const GameGrid = () => {
 
     const [games, setGames] = useState<Game[]>([]);
-    const [errors, setErrors] = useState('');
+    const [error, setError] = useState('');
 
     useEffect(() => {
         apiClient.get<FetchGameresponse>('/games')
             .then((res) => setGames(res.data.results))
-            .catch(err => setErrors(err.message));
+            .catch(err => setError(err.message));
   })
     return (
-    <div>
-            {
-                games.map(game => <li key={game.id}> {game.name} {' '} {game.rating }</li>)
-        }
-    </div>
+        <div>
+            {error && <Text fontSize='3xl' color='tomato'>We are Sorry. Something is not right with us!</Text>}
+            {games.map(game => <GameCard key={game.name} game={game} />)}
+        </div>
   )
 }
 
