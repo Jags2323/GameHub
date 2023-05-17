a996938856773e964e52f6c4324384326034307c
Vikram Rajeev Reddy Kadire <vikramrajeevreddykadire@Vikrams-MacBook-Pro.local>
2023-05-17 11:27:13 -0400

    added EC2 backend and error page

--- src/components/Testing.tsx
+++ src/components/Testing.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import useTest from '../hooks/useTest'
-import { List, ListItem } from '@chakra-ui/react';
+import { List, ListItem, Text } from '@chakra-ui/react';
 
 const Testing = () => {
 
-    const { gamesData } = useTest();
+    const { gamesData, error, isLoading } = useTest();
     
   return (
-      <div>
+    <div>
+      {error && <Text fontSize='3xl' color='tomato'>We are Sorry. Something is not right with us!</Text>}
+      
           <List>
               {gamesData.map(game => <ListItem>{game.name}</ListItem>)}
           </List>
