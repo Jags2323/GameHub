0f568838d6d131d7c02ab0f46fc8658f63f82584
JagsP23 <pullapuramjr@gmail.com>
2023-05-22 12:21:57 -0400

    deleted platforms.tsx file

--- src/components/IconList.tsx
+++ src/components/IconList.tsx
@@ -33,7 +33,7 @@ import {
     return (
       <HStack marginY={1}> 
         {platforms.map((platform) => (
-          <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500'/>
+          <Icon key={platform.id} as={iconMap[platform.slug]} color='green.500'/>
         ))}
       </HStack>
     );
