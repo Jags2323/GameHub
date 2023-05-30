859304632460c49197e4f8636993b4ecb2d39a01
JagsP23 <pullapuramjr@gmail.com>
2023-05-30 10:19:22 -0400

    added routing functionality to image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { Box, HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ThemeSwitch from "./ThemeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 interface Props {
   onSearch: (searchText: string) => void;
@@ -11,7 +12,9 @@ const NavBar = ({ onSearch }: Props) => {
   return (
     <div>
       <HStack padding="10px">
-        <Image src={logo} boxSize="60px"></Image>
+        <Link to="/" replace={true} onClick={() => window.location.reload()}>
+          <Image src={logo} boxSize="60px"></Image>
+        </Link>
         <SearchInput onSearch={onSearch} />
         <ThemeSwitch />
       </HStack>
