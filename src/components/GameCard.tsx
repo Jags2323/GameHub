5e88424a7f627af676707f0c544791946bef9cf9
JagsP23 <pullapuramjr@gmail.com>
2023-06-01 10:02:46 -0400

    Merge changes from local branch

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Game } from "../hooks/useGames";
 import {
+  Box,
   Card,
   CardBody,
   HStack,
@@ -13,6 +14,7 @@ import IconList from "./IconList";
 import { Link } from "react-router-dom";
 import { FaHeart } from "react-icons/fa";
 import { useState } from "react";
+import { animated, useSpring, config } from "@react-spring/web";
 
 interface Props {
   game: Game;
@@ -20,48 +22,69 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   const [isFavorite, setFavorite] = useState(false);
+  const [hovered, setHovered] = useState(false);
 
   const handleFavoriteClick = () => {
     setFavorite(!isFavorite);
   };
 
+  const cardAnimation = useSpring({
+    transform: hovered ? "scale(1.03)" : "scale(1)",
+    boxShadow: hovered ? "0 4px 8px rgba(0, 0, 0, 0.1)" : "none",
+  });
+
+  const favoriteButtonAnimation = useSpring({
+    opacity: isFavorite || hovered ? 1 : 0,
+    transform: isFavorite || hovered ? "scale(1)" : "scale(0)",
+    config: config.gentle,
+  });
+
+  const iconAnimation = useSpring({
+    transform: `rotateY(${isFavorite ? 180 : 0}deg)`,
+    color: isFavorite ? "red.500" : "gray.500",
+  });
+
   return (
-    <div>
-      <Card
-        borderRadius={10}
-        overflow={"hidden"}
-        key={game.id}
-        _hover={{
-          transform: "scale(1.03)",
-        }}
-      >
+    <animated.div
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      style={cardAnimation}
+    >
+      <Card borderRadius={10} overflow="hidden" key={game.id}>
         <Image src={game.background_image} />
         <CardBody>
-          <Heading fontSize="2xl">
-            <Link to={"/games/" + game.slug}>{game.name}</Link>
-          </Heading>
-          <HStack justifyContent={"space-between"}>
-            <IconList
-              platforms={game.parent_platforms?.map((p) => p.platform)}
-            />
-            <CritcScore game={game} />
-          </HStack>
-          <HStack mt={4}>
-            <Tooltip
-              label={isFavorite ? "Remove from favorites" : "Add to favorites"}
-            >
-              <IconButton
-                aria-label="Favorite"
-                icon={<FaHeart />}
-                color={isFavorite ? "red.500" : "gray.500"}
-                onClick={handleFavoriteClick}
+          <Box>
+            <Heading fontSize="2xl">
+              <Link to={"/games/" + game.slug}>{game.name}</Link>
+            </Heading>
+            <HStack justifyContent="space-between">
+              <IconList
+                platforms={game.parent_platforms?.map((p) => p.platform)}
               />
-            </Tooltip>
-            {/* Render the user login/signup component here */}
-          </HStack>
+              <CritcScore game={game} />
+            </HStack>
+            <HStack mt={4}>
+              <animated.div style={favoriteButtonAnimation}>
+                <Tooltip
+                  label={
+                    isFavorite ? "Remove from favorites" : "Add to favorites"
+                  }
+                >
+                  <animated.div style={iconAnimation}>
+                    <IconButton
+                      aria-label="Favorite"
+                      icon={<FaHeart />}
+                      color={isFavorite ? "red.500" : "gray.500"}
+                      onClick={handleFavoriteClick}
+                    />
+                  </animated.div>
+                </Tooltip>
+              </animated.div>
+            </HStack>
+          </Box>
         </CardBody>
       </Card>
-    </div>
+    </animated.div>
   );
 };
 
